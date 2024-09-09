233988eb058cac7004cb8c78d4618fb52295c5c3
mitra <mitraranjbar145@gmail.com>
2024-09-09 16:51:29 +0400

    new file

---
+++ practice.py
@@ -0,0 +1,28 @@
+def welcome():
+    name = input("Name: ")
+    print (name)
+
+def variable_practice():
+    age=19
+    days_in_a_year=365 #int
+    name="mitra" #string
+    pi=3.14 # float
+    print(age,days_in_a_year,name,pi)
+
+def prompt_and_print():
+    x= input("enter a number: ")
+    y= input("enter a number: ")
+    x=int(x)
+    y=int(y)
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+def main():
+    # variable_practice()
+    # welcome()
+    prompt_and_print()
+
+main()
+
+ 

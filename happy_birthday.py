79e1cb12038700714872b3c62fe801fd6889c624
mitra <mitraranjbar145@gmail.com>
2024-09-09 16:24:42 +0400

    first version of bday file

---
+++ happy_birthday.py
@@ -0,0 +1,6 @@
+name = input("please enter name: ")
+# print ("Name: ",name)
+month = input("please enter month: ")
+day = input("please enter day: ")
+year = input("please enter year: ")
+print(name, ", your birthday is on ", month, " ", day ," ", year)
